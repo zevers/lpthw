009b03c70cb9835de23404c7e68574babcef1203
zevers <32400192+zevers@users.noreply.github.com>
2017-10-03 15:35:23 -0700

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,34 @@
+#This will print the line
+print("I will now count my chickens:")
+
+#This will add 30 divided by 6 to 25
+print("Hens", 25 + 30 / 6)
+#This will first calculate what the remainer of 25 times 3 (75) divided by 4 is (which yields 3), and will then subtract that from 100
+print("Roosters", 100 - 25 * 3 % 4)
+
+#This just prints the line
+print("Now I will count the eggs:")
+
+#This evaluates to 1 (6-5) plus 0 (there is no remainder for 4 divided by 2) minus 1/4 and plus 6, hence 6.75
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#This prints text
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#This is a boolean evaluation of whether it is true that 5 is greater than -2
+print(3 + 2 < 5 - 7)
+
+#This first prints text, then evaluates an equation
+print("What is 3 + 2", float(3 + 2))
+print("what is 5 - 7", 5 - 7)
+
+#This prints text
+print("Oh, that's why it's False.")
+
+#This prints text
+print("How about some more.")
+
+#This first prints text, then evaluates an equation using boolean concepts
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
